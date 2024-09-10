f62f14bbd5d11cb940e52d5007fce024c37a25c6
mcanoibec <148772587+mcanoibec@users.noreply.github.com>
2024-09-10 17:31:25 +0200

    aaaaaaaaaaaaaaaaaa

--- work_directory/other_examples/ep_predictor/predictor_backend.py
+++ work_directory/other_examples/ep_predictor/predictor_backend.py
@@ -18,6 +18,8 @@ from nn_backend.conv_imgs import conv_imgs
 from os import listdir
 from os.path import isfile, join
 import re
+from tqdm import tqdm
+from nn_backend.extras import simulation_table_import
 
 class fc_input:
     def __init__(self, topography_file, curves_file, ref_file, cutout_size):
@@ -33,30 +35,49 @@ class fc_input:
         self.nu_dim=nu_dim
         self.cutout_size=cutout_size
 
-    def get_dcdz(self, maxlen, curve_height_setpoint, curve_nsamples, use_lifts):
+    def get_dcdz(self, maxlen, curve_height_setpoint, curve_nsamples, curves_max_limit, use_lifts):
+        zs=np.round((np.logspace(start=0,stop=math.log10(maxlen),num=curve_nsamples,endpoint=False)+curve_height_setpoint)).astype(int)
+        samples = []
+        [samples.append(x) for x in zs if x not in samples]
+        self.curves_max_limit=curves_max_limit
         if use_lifts:
             if use_lifts:
                 og_dim=self.og_dim
+                nu_dim=self.nu_dim
+                zetas=np.arange(curves_max_limit)
                 lifts = [f for f in listdir(self.curves_path) if isfile(join(self.curves_path, f))]
-                samples = [int(re.search(r'\d+', filename).group()) for filename in lifts]
-                samples.sort()
+                lift_heights = [int(re.search(r'\d+', filename).group()) for filename in lifts]
+                lift_heights.sort()
                 lift_samples=np.zeros((og_dim,og_dim,1))
-                for l in samples:
+                for l in lift_heights:
                     file, ax_ep, ay_ep= import_xyz_img(rf'{self.curves_path}\LiftPower_{l}_.txt')
                     file=np.expand_dims(file,axis=-1)
                     lift_samples=np.concatenate((lift_samples,file), axis=-1)
-        
-                curves_sampled=lift_samples[:,:,1:]
-                curves_sampled=matrix_cut(curves_sampled, self.nu_dim)
+                lift_samples=lift_samples[:,:,1:]
+
+                curves_sampled=matrix_cut(lift_samples, nu_dim)
+                curves_sampled=curves_sampled.reshape(nu_dim*nu_dim,len(lifts))
+                #curves_sampled=curves_sampled[:3]
+                b=np.zeros((curves_max_limit,1))
+
+                zzz=np.array(lift_heights)
+                projection = np.full((len(curves_sampled),len(zetas)), np.nan)
+                for h in tqdm(np.arange(len(curves_sampled))):
+                    ccc=curves_sampled[h,:]
+                    indices = np.where(np.isin(zetas, zzz.astype(int)))[0]
+                    projection[h, indices] = ccc[np.isin(np.round(zzz), zetas)]
+                projection=pd.DataFrame(projection)
+                projection=projection.interpolate(axis=1)
+                projection=projection.bfill(axis=1)
+                curves_sampled=np.array(projection).reshape(nu_dim,nu_dim,curves_max_limit)
+                curves_sampled=curves_sampled[:,:,samples]
+
+                
         else:
             curves_projected=pd.DataFrame(np.load(rf'{self.curves_path}'))
             curves_projected=curves_projected.interpolate(axis=1)
             curves_projected=curves_projected.bfill(axis=1)
 
-            zs=np.round((np.logspace(start=0,stop=math.log10(maxlen),num=curve_nsamples,endpoint=False)+curve_height_setpoint)).astype(int)
-            samples = []
-            [samples.append(x) for x in zs if x not in samples]
-        
             curves=(np.array(curves_projected)).reshape(128,128,len(curves_projected.iloc[0,:]))
             curves=matrix_cut(curves, self.nu_dim)
             curves_sampled=curves[:,:,samples]
@@ -72,10 +93,10 @@ class fc_input:
         self.topography=self.t_mat.reshape(self.nu_dim**2)
 
         self.y=pd.DataFrame(np.reshape(self.y_mat,self.nu_dim**2))
+
+    def get_x(self, maxlen, curve_height_setpoint, curve_nsamples, curves_max_limit, see_data, use_lifts):
         
-    def get_x(self, maxlen, curve_height_setpoint, curve_nsamples, see_data, use_lifts):
-        
-        self.get_dcdz(maxlen, curve_height_setpoint, curve_nsamples,use_lifts)
+        self.get_dcdz(maxlen, curve_height_setpoint, curve_nsamples, curves_max_limit, use_lifts)
         self.get_vectors()
         X_mat=np.concatenate((self.t_mat, self.dcdz_sampled), axis=2)
 
@@ -88,8 +109,8 @@ class fc_input:
         self.x=X
         self.x_mat=X_mat
         if see_data:
-            max_v=(np.min(X_mat)+np.max(X_mat))/2+np.max(X_mat)/2
-            min_v=(np.min(X_mat)+np.max(X_mat))/2-np.max(X_mat)/2
+            max_v=(np.min(X_mat[:,:,1])+np.max(X_mat[:,:,1]))/2+np.max(X_mat[:,:,1])/2
+            min_v=(np.min(X_mat[:,:,1])+np.max(X_mat[:,:,1]))/2-np.max(X_mat[:,:,1])/2
             fig=plt.figure(figsize=(12,10))
             ax=fig.add_subplot(5,3,1)
             pcm=ax.imshow(X_mat[:,:,0], cmap='hot')
@@ -103,7 +124,7 @@ class fc_input:
                 ax.set_title("Lift; "+str(h))
                 plt.colorbar(pcm, label="$dC/dz$")
                 ax.axis('off')
-    
+
 
     def apply_masks(self, mask_indexes, mask_from_topo_indexes):
         if hasattr(self, 'x'):
@@ -182,6 +203,59 @@ class conv_input:
         self.x_cutouts_masked=Cv_mat_masked
         self.x_cutouts_masked_from_topo=Cv_mat_masked_from_topo
 
+class sim_tables:
+    def __init__(self, fc, conv, sim_tables_folder):
+        self.fc=fc
+        og_dim=fc.og_dim
+        nu_dim=fc.nu_dim
+        tab=simulation_table_import(sim_tables_folder, fc.curves_max_limit)
+        nu_xs=[]
+        nu_ys=[]
+        nu_idxs=[]
+        for i in np.arange(len(tab)):
+            tab_mat=np.zeros((og_dim,og_dim))
+            iy=int(tab.at[i,'y'])
+            ix=int(tab.at[i,'x'])
+            tab_mat[iy,ix]=1
+            tab_mat=np.rot90(tab_mat,k=3)
+            tab_mat=matrix_cut(tab_mat,nu_dim)
+            nu_y, nu_x = np.where(tab_mat == 1)
+            nu_idx= np.ravel_multi_index((nu_y,nu_x),(nu_dim,nu_dim))
+            
+            nu_xs.append(nu_x[0])
+            nu_ys.append(nu_y[0])
+            nu_idxs.append(nu_idx[0])
+        tab.insert(0,'nu x', nu_xs)
+        tab.insert(0,'nu y', nu_ys)
+        tab.insert(0,'nu idx', nu_idxs)
+        y_tab=pd.DataFrame(tab['ep'])
+        tab_index=tab['nu idx']
+        tab_curves_sampled=tab.iloc[:,6:]
+        tab_curves_sampled=tab_curves_sampled.iloc[:,fc.sample_heights]
+        tab_T=fc.topography[tab_index].reshape(len(tab_index),1)
+        X_tab=np.concatenate((tab_T, tab_curves_sampled), axis=1)
+        self.x_tab=X_tab
+        self.x_tab=y_tab
+
+        self.conv=conv
+        topo_n_mat=self.conv.topography_norm
+        cutout_size=self.conv.cutout_size
+        y_coord, x_coord= np.unravel_index(tab_index, (nu_dim, nu_dim))
+        tab_index_2=np.transpose(np.column_stack((y_coord, x_coord)))
+        tab_index_2[0]=tab_index_2[0]+int((cutout_size-1)/2)
+        tab_index_2[1]=tab_index_2[1]+int((cutout_size-1)/2)
+        Cv_tab_mat=conv_imgs(topo_n_mat[:,:,0], cutout_size, tab_index_2)
+        Cv_tab_mat=np.expand_dims(Cv_tab_mat, axis=-1)
+        self.conv_tab=Cv_tab_mat
+
+    def normalize(self):
+        if hasattr(self, 'x_tab'):
+            curve_scale=StandardScaler()
+            curve_scale.fit(self.fc.x_masked)
+            self.x_tab_norm=pd.DataFrame(curve_scale.transform(self.x_tab), columns=self.fc.x.columns)
+        else:
+            print("Error: X matrix not found. Use get_x() to obtain")
+
 class preprocessing:
     def __init__(self, topography_file, curves_file, ref_file, cutout_size):
         self.topography_path = topography_file
@@ -219,18 +293,22 @@ class preprocessing:
         self.mask_indexes=cell_index
 
         fig=plt.figure(figsize=(10,7))
-        ax=fig.add_subplot(1,2,1)
+        ax=fig.add_subplot(1,3,1)
+        ax.imshow(self.t_mat, cmap='hot')
+        ax.set_title('Topography')
+        ax.axis('off')
+        ax=fig.add_subplot(1,3,2)
         ax.imshow(bin, cmap='gray')
         ax.set_title('Mask from Topography')
         ax.axis('off')
-        ax=fig.add_subplot(1,2,2)
+        ax=fig.add_subplot(1,3,3)
         ax.imshow(mask_ep_im)
         ax.axis('off')
         ax.set_title('Final Train Mask')
-    def get_fc(self, maxlen=1713, curve_height_setpoint=20, curve_nsamples=12, see_data=0, use_lifts=0):
+    def get_fc(self, maxlen=1713, curve_height_setpoint=20, curve_nsamples=12, curves_max_limit=1000, see_data=0, use_lifts=0):
         if hasattr(self, 'mask_indexes'):
             fc = fc_input(self.topography_path, self.curves_path, self.y_path, self.cutout_size)
-            fc.get_x(maxlen, curve_height_setpoint, curve_nsamples, see_data, use_lifts)
+            fc.get_x(maxlen, curve_height_setpoint, curve_nsamples, curves_max_limit, see_data, use_lifts)
             fc.apply_masks(self.mask_indexes, self.mask_from_topo_indexes)
             fc.normalize()
             self.fc=fc
@@ -247,7 +325,10 @@ class preprocessing:
             self.conv=conv
         else:
             print("Error: Mask not found. Use get_mask() to obtain")
-    
+    def get_tables(self, sim_tables_folder):
+        tabs=sim_tables(self.fc, self.conv, sim_tables_folder)
+        #tabs.normalize() ISUEEEEEES
+        self.tabs=tabs
 class ep_prediction:
     def __init__(self, preproc_data, pctg, augment=1):
         fc, conv, y = preproc_data.fc.x_masked_norm,preproc_data.conv.x_cutouts_masked,preproc_data.fc.y_masked
